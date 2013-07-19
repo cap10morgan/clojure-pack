f28d4d1e69d9787ab9f9718ef5de0420b11fab75
Wes Morgan <cap10morgan@gmail.com>
2013-07-19 15:43:01 -0600

    move dependencies to MELPA packages where possible

---
+++ clojure-pack-pkg.el
@@ -0,0 +1,14 @@
+(define-package "clojure-pack" "1.0.0"
+  "Emacs Live pack for Clojure hacking. Includes: paredit, nrepl, ac-nrepl, auto-complete, clojure-mode, mic-paren, rainbow-delimiters, popup-el, and more."
+  '((paredit "20130709.2057")
+    (nrepl "20130330.1126")
+    (ac-nrepl "20130420.929")
+    (nrepl-eval-sexp-fu "20130220.2142")
+    (auto-complete "20130503.2013")
+    (clojure-mode "20130525.1027")
+    (mic-paren "20120716.1647")
+    (rainbow-delimiters "20130307.340")
+    (popup "20130324.1305")
+    (align-cljlet "20121118.1041")
+    (fuzzy "20120323.1044")
+    (uuid "20120910.951")))
